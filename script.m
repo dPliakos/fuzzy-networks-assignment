c0ee0e285f71f86db06ff67d56440acc163dbed2
Dimitris Pliakos <dimitrispl96@gmail.com>
2020-11-03 21:44:20 +0200

    Add some comments

--- script.m
+++ script.m
@@ -2,13 +2,22 @@ file = '/mnt/secondary/home/dpliakos/backups/documents_no_code/tei/sem11/fuzzNet
 fis_file = '/mnt/secondary/home/dpliakos/backups/documents_no_code/tei/sem11/fuzzNetworks/assignment1/format.fis'
 pkg load fuzzy-logic-toolkit
 
+# Read data
+
 data = csvread(file)
 
 x = data(:,1:4)
 t = data(:,5)
 
 
+# prepare the anfis environment
 readfis(fis_file)
+
+
+# Execute the computation
+
+
+# Read output
 
 
 
