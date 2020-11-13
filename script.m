91084530fed5e1bd71241513a2bf262af9305057
Dimitris Pliakos <dimitrispl96@gmail.com>
2020-11-13 21:47:07 +0200

    Run with random weights

--- script.m
+++ script.m
@@ -3,82 +3,84 @@ fis_file = '/mnt/secondary/home/dpliakos/backups/documents_no_code/tei/sem11/fuz
 pkg load fuzzy-logic-toolkit
 
 # Read data
-
-data = csvread(file)
-
+data_raw = csvread(file)
+data = data_raw(:,:)
 x = data(:,1:4)
 t = data(:,5)
-
-
-# prepare the anfis environment
 fis = readfis(fis_file)
 
 
-# Execute the computation
+% 1960
+train_length = round(rows(x)*0.7) - 1
+test_length = rows(x) - train_length
+xtrain = x(1:train_length,:)
+ttrain = t(1:train_length,:)
 
-# Attempt to run defuzzify
-y = evalfis(x, fis)
+xtest = x(train_length+1:rows(x),:)
+ttest = t(train_length+1:rows(x),:)
 
-# Read output
+% # gia kathe lanthasmeno apatelsma
+max_epoch = 4
+
+params = []
+counts = []
+result = []
+for epoch = 1:max_epoch  
+  for rule_index = 1:columns(fis.rule)
+    fis.rule(rule_index).weight = randi([0 1])
+  end
+  params(epoch,:) = [fis.rule.weight]
+
+  y = evalfis(xtrain, fis)
+
+  result = []
+  for i = 1:rows(y)
+    [val, a] = max(y(i,:))
+    result(i) = a-1
+  end
+  
+  count_true = 0
+  for i = 1:columns(result)
+    if result(i) == ttrain(i),
+      count_true = count_true + 1
+    endif
+  end
+
+  counts(epoch) = count_true
+end
+
+
+[best_res best_index] = max(counts)
+
+best_res
+best_index
+
+params(best_index,:)
+
+
+for i=1:columns(fis.rule)
+  fis.rule(i).weight = params(best_index,i)
+end
+
+y = evalfis(xtest, fis)
+
 
 result = []
 for i = 1:rows(y)
   [val, a] = max(y(i,:))
-  result = [result; a-1]
+  result(i) = a-1
 end
 
-
-
-result
-
-
 count_true = 0
-
-for i = 1:rows(result)
-  if result(i) == t(i),
-    disp("yey!")
+for i = 1:columns(result)
+  if result(i) == ttrain(i),
     count_true = count_true + 1
   endif
 end
 
 
-disp("Number of yeys = ")
-disp(count_true)
-disp(rows(t))
-disp(rows(t) - count_true)
+disp("rows(xtest):")
+rows(xtest)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%  BNackup
-
-% flag=0
-% for i=1:rows(output)
-%   if output(i,1) == 1,
-%     flag=1
-%   endif;
-% end;
-
-% if flag == 1,
-%   disp("Nai re pousti")
-% endif
+disp("count_true: ")
+count_true
